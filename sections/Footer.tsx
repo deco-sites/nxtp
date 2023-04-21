821ec35673bffe456cb39d62ba5c9053d703e0e4
Ricardo Henrique Gonzaga <ricardo.gonzaga@agenciam3.com>
2023-04-21 17:21:12 -0300

    feat: add footer phone

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -37,7 +37,7 @@ export default function Footer({ social, logo, certifieds, reservedRights }: Pro
 
                 {/* Tablet view */}
 
-                <div class="flex items-center justify-between h-[80%] border-b border-solid border-footer lg:hidden">
+                <div class="hidden sm:flex items-center justify-between h-[80%] border-b border-solid border-footer lg:hidden">
                     <a class="cursor-pointer" dangerouslySetInnerHTML={{ __html: logo }} />
 
                     <div class="flex flex-col justify-around items-center">
@@ -46,6 +46,27 @@ export default function Footer({ social, logo, certifieds, reservedRights }: Pro
                     </div>
 
                     <div class="flex flex-col items-center">
+                        {social?.map((social) => {
+                            return (
+                                <h6 class="text-footertablet text-heading-1 m-[10px] mb-0 hover:text-footer-hover cursor-pointer">
+                                    <a href={social.link}>{social.text}</a>
+                                </h6>
+                            )
+                        })}
+                    </div>
+                </div>
+
+                {/* Phone view */}
+
+                <div class="flex flex-col items-center justify-between h-[80%] mb-[30px] sm:hidden">
+                    <a class="cursor-pointer" dangerouslySetInnerHTML={{ __html: logo }} />
+
+                    <div class="flex flex-col justify-around items-center">
+                        <button class="text-footertablet leading-[1.5] text-heading-1 mb-4 mr-4 rounded-[30px] pr-12 pl-8 py-[0.375rem] cursor-pointer transition ease-in-out delay-50 hover:bg-white hover:text-default-inverse">Contact Us</button>
+                        <button class="text-footertablet leading-[1.5] text-heading-1 flex items-center border border-solid border-white rounded-[30px] pr-6 pl-8 pr-6 py-1 h-[3.5rem] cursor-pointer transition ease-in-out delay-50 hover:bg-white hover:text-default-inverse">Sign up for newsletter <img class="ml-12 h-[24px]" width={13} height={24} src="/arrow-right.svg" /></button>
+                    </div>
+
+                    <div class="flex items-center">
                         {social?.map((social) => {
                             return (
                                 <h6 class="text-footertablet text-heading-1 m-[10px] mb-0 hover:text-footer-hover cursor-pointer">
@@ -80,12 +101,31 @@ export default function Footer({ social, logo, certifieds, reservedRights }: Pro
                 </div>
 
                 {/* Tablet view */}
-                <div class="flex py-6 text-sans lg:hidden">
+                <div class="hidden sm:flex py-6 text-sans lg:hidden">
                     <div class="py-6 px-2 flex justify-center w-[75%]">
                         <span class="font-heading-2 relative right-[-15%]">{reservedRights}</span>
                     </div>
 
                     <div class="flex justify-end w-[25%]">
+                        {certifieds?.map((certified) => {
+                            return (
+                                <div class="flex items-center mr-8 last:mr-0">
+                                    <img
+                                        src={certified.src}
+                                        width={certified.width}
+                                        height={certified.height}
+                                    />
+                                </div>
+                            )
+                        })}
+                    </div>
+
+                </div>
+
+                {/* Phone view */}
+                <div class="flex text-sans sm:hidden">
+
+                    <div class="flex justify-center w-full">
                         {certifieds?.map((certified) => {
                             return (
                                 <div class="flex items-center mr-8 last:mr-0">
